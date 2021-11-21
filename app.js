7970bb7f49c56c15159a41756585e7f9a247836f
debajitsaha89 <81859187+debajitsaha89@users.noreply.github.com>
2021-11-21 14:27:53 +0530

    done

---
+++ app.js
@@ -0,0 +1,11 @@
+// function addition()
+// {
+// var x=   Number( document.getElementById("fn1").value);
+// var y=  Number( document.getElementById("sn2").value);
+// let z=x+y;
+// document.getElementById("para").innerText=z;
+// }
+function addition(){
+var res=Number(document.getElementById("fn1").value)+ Number(document.getElementById("sn2").value);
+document.getElementById("para").innerText=res;
+}
